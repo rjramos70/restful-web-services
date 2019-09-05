d74b30e091d00fc7998b7fc81ab152b24df284ad
Renato Jiquiriçá Ramos <rjramos70@Renatos-MacBook-Pro-54283.local>
2019-09-04 23:54:15 -0300

    Update da classe HelloWorldController.java implementando novo método que retorno um Bean HelloWorldBean parseado para JSon

--- src/main/java/com/in28minutes/rest/webservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/controller/HelloWorldController.java
@@ -1,17 +1,23 @@
 package com.in28minutes.rest.webservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.rest.webservices.bean.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
 	
-	// @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
 	
 }
