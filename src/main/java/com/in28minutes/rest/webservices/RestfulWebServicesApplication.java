c09c3131eb756305fed1a81bcae693aea49f59d3
Renato Jiquiriçá Ramos <rjramos70@Renatos-MacBook-Pro-54283.local>
2019-09-07 18:17:41 -0300

    Correção do erro encontrado ao implementar internacionalização

--- src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
@@ -4,9 +4,10 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -18,9 +19,9 @@ public class RestfulWebServicesApplication {
 
 	@Bean
 	public LocaleResolver localResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-		localeResolver.setDefaultLocale(Locale.US);
-		return localResolver();
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
 	}
 	
 	@Bean
