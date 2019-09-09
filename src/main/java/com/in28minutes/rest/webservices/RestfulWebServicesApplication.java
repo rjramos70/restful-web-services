32b215dd9ac604bb4e6720cf97dcae1f9eb2f389
Renato Ramos <renato.ramos@mss.modulo.com.br>
2019-09-09 18:11:44 -0300

    Atualização do projeto até a implemetação de filtragem dinâmica dos serviços RESTFul

--- src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/RestfulWebServicesApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
+// @ComponentScan(basePackages = {"com.in28minutes.rest.webservices"})
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
